4913f5d1f6fc362cabdaeb2a58f4c71278d9c795
rishi <rishi.rgt@gmail.com>
2024-03-28 08:49:20 +0000

    Refactored App.tsk

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
-      showGraph: false,
+      showGraph: false
     };
   }
 
@@ -45,7 +45,7 @@ class App extends Component<{}, IState> {
       DataStreamer.getData((serverResponds: ServerRespond[]) => {
         this.setState({
           data:serverResponds,
-          showGraph: true,
+          showGraph: true
 
         });
       });
